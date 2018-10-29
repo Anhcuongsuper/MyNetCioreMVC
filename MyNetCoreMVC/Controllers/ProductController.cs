5a856a3674a7948018f03d1539a20606e1fb5adc
anhcuongsuper <cuongnld00635@fpt.edu.vn>
2018-10-29 17:30:41 +0700

    Upload file

---
+++ MyNetCoreMVC/Controllers/ProductController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MyNetCoreMVC
+{
+    public class ProductController : Controller
+    {
+
+        //[Route("NgoLyCuong/Index/{id:int}")]
+        //public IActionResul IndexActionResul(int id)
+        //{
+        //    return new JsonResult(new MyCustomer()
+        //    {
+        //        Name = id + ""
+        //    });
+        //}
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult GetList()
+        {
+            return View();
+        }
+        public IActionResult Create(string name, string price)
+        {
+            ViewData["name"] = name;
+            ViewData["price"] = name;
+
+            return View();
+        }
+        public IActionResult Update()
+        {
+            return View();
+
+        }
+        public IActionResult Delete(int id)
+        {
+
+
+            return new JsonResult(new Product()
+            {
+
+                _id = id
+            });
+        }
+    }
+}
+
